693ebe8f26148039107dba922c7e78661144b620
Tausif Shaikh <shaikhtausif089@gmail.com>
2025-02-09 18:32:24 +0530

    added Cursor.jsx

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -17,7 +17,7 @@ const Cursor = () => {
 
   return (
     <motion.div
-      className="fixed w-5 h-5 bg-blue-500 rounded-full pointer-events-none z-[999] -translate-x-1/2 -translate-y-1/2"
+      className="fixed md:visible hidden w-5 h-5 bg-blue-500 rounded-full pointer-events-none z-[999] -translate-x-1/2 -translate-y-1/2"
       animate={{ x: position.x, y: position.y }}
       transition={{ duration: 0.6, ease: "backOut" }}
     />
